cfcaada023397f1956bd577f022f323915b4efa0
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-07-17 13:07:12 +0300

    add icons

--- Analogy.LogViewer.PlainTextParser/Properties/Resources.Designer.cs
+++ Analogy.LogViewer.PlainTextParser/Properties/Resources.Designer.cs
@@ -273,6 +273,26 @@ namespace Analogy.LogViewer.PlainTextParser.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap New_16x16 {
+            get {
+                object obj = ResourceManager.GetObject("New_16x16", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap New_32x32 {
+            get {
+                object obj = ResourceManager.GetObject("New_32x32", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Next_16x16 {
             get {
                 object obj = ResourceManager.GetObject("Next_16x16", resourceCulture);
