85451a416f927352bde1b287e92392a55b9106c3
Lior Banai <liorbanai@gmail.com>
2020-09-04 13:36:10 +0300

    update api

--- Analogy.LogViewer.PlainTextParser/AnalogyColumnsMatcherUC.cs
+++ Analogy.LogViewer.PlainTextParser/AnalogyColumnsMatcherUC.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Analogy.Interfaces;
 using Analogy.Interfaces.DataTypes;
+using Analogy.CommonUtilities.ExtensionMethods;
 
 namespace Analogy.LogViewer.NLogProvider
 {
